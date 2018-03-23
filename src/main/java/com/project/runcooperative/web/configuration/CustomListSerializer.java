cd764a8b65cd07a10c72c408dfbba05921c6388c
eshiett1995 <eshiett1995@gmail.com>
2018-03-23 18:52:14 +0100

    all i have done

---
+++ src/main/java/com/project/runcooperative/web/configuration/CustomListSerializer.java
@@ -0,0 +1,7 @@
+package com.project.runcooperative.web.configuration;
+
+/**
+ * Created by Oto-obong on 20/03/2018.
+ */
+public class CustomListSerializer {
+}
---
+++ src/main/java/com/project/runcooperative/web/configuration/InterceptorConfiguration.java
@@ -0,0 +1,40 @@
+package com.project.runcooperative.web.configuration;
+
+import com.project.runcooperative.web.interceptors.GenericInterceptors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Oto-obong on 01/02/2018.
+ */
+
+@Configuration
+public class InterceptorConfiguration extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    GenericInterceptors genericInterceptors;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(genericInterceptors).excludePathPatterns("/")
+                .excludePathPatterns("/login/authenticate")
+                .excludePathPatterns("/login/authenticatecustomer")
+                .excludePathPatterns("/populate")
+                .excludePathPatterns("/customerlogin")
+                .excludePathPatterns("/loan/regular-add")
+                .excludePathPatterns("/home")
+                .excludePathPatterns("/customer/change-password")
+                .excludePathPatterns("/LoginCustomerController")
+                .excludePathPatterns("/customer-home")
+                .excludePathPatterns("/approve/loans")
+                .excludePathPatterns("/view/loans/{id}")
+                .excludePathPatterns("/loan/emergency/add")
+                .excludePathPatterns("/{email}/loans")
+                .excludePathPatterns("/error");
+
+    }
+
+
+}
