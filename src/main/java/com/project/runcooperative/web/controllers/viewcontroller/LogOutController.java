352f1ce5a5b48f227107d41861b3e3d5fa6c94ad
eshiett1995 <eshiett1995@gmail.com>
2018-03-23 19:01:16 +0100

    all i have done

---
+++ src/main/java/com/project/runcooperative/web/controllers/viewcontroller/LogOutController.java
@@ -0,0 +1,40 @@
+package com.project.runcooperative.web.controllers.viewcontroller;
+
+import com.project.runcooperative.web.entities.PersonnelEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by Oto-obong on 11/02/2018.
+ */
+
+@Controller
+public class LogOutController {
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String SignOut(HttpServletResponse response){
+
+            Cookie cookie = new Cookie("credentials",null);
+
+            cookie.setSecure(false);
+            cookie.setHttpOnly(true);
+            cookie.setMaxAge(6000);
+            cookie.setDomain("localhost");
+            cookie.setPath("/");
+
+            response.addCookie(cookie);
+
+            return "redirect:/";
+
+
+    }
+}
