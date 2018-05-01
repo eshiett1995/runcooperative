77e1ac74da269f31f7bdf9b9d690e737667a8a6f
eshiett1995 <eshiett1995@gmail.com>
2018-05-01 10:22:07 +0100

    commit at home

--- src/main/java/com/project/runcooperative/web/controllers/viewcontroller/PersonneltableController.java
+++ src/main/java/com/project/runcooperative/web/controllers/viewcontroller/PersonneltableController.java
@@ -1,11 +1,18 @@
 package com.project.runcooperative.web.controllers.viewcontroller;
 
+import com.project.runcooperative.web.controllers.restcontroller.models.ResponseModel;
+import com.project.runcooperative.web.entities.CustomerEntity;
+import com.project.runcooperative.web.entities.PersonnelEntity;
 import com.project.runcooperative.web.services.PersonnelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PersonneltableController {
@@ -19,5 +26,22 @@ public class PersonneltableController {
         model.addAttribute("personnels", personnelService.GetAll());
 
         return "personneltable";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/delete/personnel/{personnelId}", method = RequestMethod.GET)
+    public ResponseEntity<ResponseModel> DeleteCustomer(@PathVariable Long personnelId, Model model){
+
+        PersonnelEntity personnelEntity = personnelService.GetPersonnelById(personnelId);
+
+        personnelService.delete(personnelId);
+
+        ResponseModel responseModel = new ResponseModel();
+
+        responseModel.setSuccessful(true);
+
+        responseModel.setResponseMessage("the customer " + personnelEntity.getFirstname() + " " + personnelEntity.getLastname() + " has been successfully deleted");
+
+        return new ResponseEntity<>(responseModel, HttpStatus.OK);
     }
 }
