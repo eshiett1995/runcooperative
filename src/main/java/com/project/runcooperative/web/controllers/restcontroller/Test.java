ada3fe82482f41f109b4732d5f91dff150a8e714
lloydd969 <35634267+lloydd969@users.noreply.github.com>
2018-05-01 22:41:12 +0100

    loansview

--- src/main/java/com/project/runcooperative/web/controllers/restcontroller/Test.java
+++ src/main/java/com/project/runcooperative/web/controllers/restcontroller/Test.java
@@ -75,6 +75,7 @@ public class Test {
         account.setAccountholder(runcooperative);
         account.setAccountNumber("0000");
 
+
         accountEntities.add(account);
 
         SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
@@ -89,6 +90,7 @@ public class Test {
         runcooperative.setPassword("hihi");
         runcooperative.setLastname("Account");
         runcooperative.setAccounts(accountEntities);
+        runcooperative.setSalary("null");
 
 
 
